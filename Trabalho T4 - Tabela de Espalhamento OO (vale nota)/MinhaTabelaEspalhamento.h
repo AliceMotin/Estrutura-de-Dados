5a7771b7c0aac0b2e5d81b33d3973ad16349f2e3
Alice <112569754+AliceMotin@users.noreply.github.com>
2023-12-20 10:31:12 -0300

    Update MinhaTabelaEspalhamento.h

--- Trabalho T4 - Tabela de Espalhamento OO (vale nota)/MinhaTabelaEspalhamento.h
+++ Trabalho T4 - Tabela de Espalhamento OO (vale nota)/MinhaTabelaEspalhamento.h
@@ -46,12 +46,6 @@ class MinhaTabelaEspalhamento final:
 
         virtual std::size_t quantidade() const
         {
-            //int tmp = 0;
-            //while(!this->tabela.empty())
-            //{
-              //  tmp++;
-            //}
-            //return tmp;
             return totalElementos;
         }
     
@@ -71,27 +65,12 @@ class MinhaTabelaEspalhamento final:
         }
     
     private:
-        /**
-         * @brief Calcula a representação numérica de um tipo integral.
-         * 
-         * @tparam U O tipo integral.
-         * @param integral Um valor integral.
-         * @return Um inteiro calculado através de static_cast<std::size_t>(integral)
-         */
         template<typename U>
         std::size_t codigoEspalhamento(U integral) const
         {
             return static_cast<size_t>(integral);
-            //return 0;//Implemente aqui. Dica use std::is_integral_v<U> para garantir que U é um tipo integral
         }
-        
-    
-        /**
-         * @brief Calcula a representação numérica de um string.
-         * 
-         * @param s um string.
-         * @return Um inteiro calculado conforme s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1].
-         */
+
         std::size_t codigoEspalhamento(std::string const& string) const
         {
             size_t hash = 0;
